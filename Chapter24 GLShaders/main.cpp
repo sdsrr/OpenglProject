ca2a0b21dfa401592f7da59bdd1c9f5dffce0c92
sdsrr <498524722@qq.com>
2019-09-17 21:48:15 +0800

    chapter23/24
    chapter23/24

---
+++ Chapter24 GLShaders/main.cpp
@@ -0,0 +1,74 @@
+#include "../Tools/Header/ShaderMgr.h"
+#include "../Tools/Header/Tools.h"
+#include "../Tools/Header/Camera.h"
+#include "../Tools/Header/GameObject.h"
+#include "../Tools/Header/FreetypeFont.h"
+#include "../Tools/Header/Light.h"
+#include "../Tools/Header/CommonStruct.h"
+
+BaseShaderParam param;
+GLShaderManager glShaderMgr;
+ShaderMgr* shaderMgr;
+NormalCamera normalCamera;
+
+
+GLfloat angle;
+GLBatch triangle;
+
+void Display(void)
+{
+    glClearColor(1,1,1,1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSwapBuffers();
+}
+
+void OnStartUp()
+{
+    glShaderMgr.InitializeStockShaders();
+    shaderMgr = ShaderMgr::GetInstance();
+    normalCamera.OnInit(640, 480, 50, 1, 2);
+}
+
+void OnShutUp()
+{
+    shaderMgr->OnUnInit();
+    normalCamera.OnUnInit();
+}
+
+void Idle(void) {glutPostRedisplay();}
+void KeyboardFn(unsigned char key, int x, int y) {normalCamera.KeyboardFn(key, x, y);}
+void MouseClick(int button, int action, int x, int y) {normalCamera.MouseClick(button, action, x, y);}
+void MotionFunc(int mouse_x, int mouse_y) {normalCamera.MotionFunc(mouse_x, mouse_y);}
+void Resize(int w, int h) {normalCamera.Resize(w, h);}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("gl shaders");
+
+
+    glutKeyboardFunc(KeyboardFn);
+    glutReshapeFunc(Resize);
+    glutMotionFunc(MotionFunc);
+    glutMouseFunc(MouseClick);
+    glutDisplayFunc(Display);
+    glutIdleFunc(Idle);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    if (glewInit() != GLEW_OK)
+    {
+        printf("glew init error...\n");
+        return 0;
+    }
+
+    OnStartUp();
+    glutMainLoop();
+    OnShutUp();
+
+    return EXIT_SUCCESS;
+}
